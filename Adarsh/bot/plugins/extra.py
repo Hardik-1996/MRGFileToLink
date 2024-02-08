745bcec7759f4f1ce9516764867220e0d94c0c51
parmar Hardik <158484209+Hardik-1996@users.noreply.github.com>
2024-02-08 23:23:34 +0530

    extra.py

--- Adarsh/bot/plugins/extra.py
+++ Adarsh/bot/plugins/extra.py
@@ -63,7 +63,7 @@ async def start(bot, update):
     
 @StreamBot.on_message(filters.command("list"))
 async def list(l, m):
-    LIST_MSG = "Hi! {} Here is a list of all my commands \n \n 1 . `start` \n 2. `help` \n 3. `login` \n 4.`follow` \n 5. `ping` \n 6. `status` \n 7. `DC` this tells your telegram dc \n 8. `maintainers` "
+    LIST_MSG = "Hi! {} Here is a list of all my commands \n \n 1. `start` \n 2. `help` \n 3. `login` \n 4. `follow` \n 5. `ping` \n 6. `status` \n 7. `DC` this tells your telegram dc \n 8. `maintainers` "
     await l.send_message(chat_id = m.chat.id,
         text = LIST_MSG.format(m.from_user.mention(style="md"))
         
